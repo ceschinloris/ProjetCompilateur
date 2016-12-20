9b8eebcd03e4d08c7099b4d6913f0404779411b4
loris ceschin <loris.ceschin@he-arc.ch>
2016-12-20 14:32:39 +0100

    lex done

--- lexProjet.py
+++ lexProjet.py
@@ -2,6 +2,7 @@ import ply.lex as lex
 
 reserved_words = (
     'while',
+    'for',
     'if',
     'else',
     'echo'
@@ -10,21 +11,28 @@ reserved_words = (
 tokens = (
              'NUMBER',
              'ADD_OP',
+             'SUB_OP',
              'MUL_OP',
+             'DIV_OP',
+             'INCREMENT',
+             'DECREMENT',
+             'ASSIGN',
+             'ASSIGN_ADD',
+             'ASSIGN_SUB',
+             'ASSIGN_MUL',
+             'ASSIGN_DIV',
+             'COMP_EQUALS',
+             'COMP_NOT_EQUALS',
+             'COMP_GT',
+             'COMP_GT_EQUALS',
+             'COMP_LT',
+             'COMP_LT_EQUALS',
              'IDENTIFIER',
+             'STRING',
+             'VARIABLE',
          ) + tuple(map(lambda s: s.upper(), reserved_words))
 
-literals = '();=<>.!{}$"\''
-
-
-def t_ADD_OP(t):
-    r'[+-]'
-    return t
-
-
-def t_MUL_OP(t):
-    r'[*/]'
-    return t
+literals = '();.{}"'
 
 
 def t_NUMBER(t):
@@ -37,10 +45,105 @@ def t_NUMBER(t):
     return t
 
 
+def t_INCREMENT(t):
+    r'\+\+'
+    return t
+
+
+def t_DECREMENT(t):
+    r'--'
+    return t
+
+
+def t_ASSIGN_ADD(t):
+    r'\+='
+    return t
+
+
+def t_ASSIGN_SUB(t):
+    r'-='
+    return t
+
+
+def t_ASSIGN_MUL(t):
+    r'\*='
+    return t
+
+
+def t_ASSIGN_DIV(t):
+    r'/='
+    return t
+
+
+def t_COMP_EQUALS(t):
+    r'=='
+    return t
+
+
+def t_COMP_NOT_EQUALS(t):
+    r'!='
+    return t
+
+
+def t_COMP_LT_EQUALS(t):
+    r'<='
+    return t
+
+
+def t_COMP_GT_EQUALS(t):
+    r'>='
+    return t
+
+
+def t_ADD_OP(t):
+    r'\+'
+    return t
+
+
+def t_SUB_OP(t):
+    r'-'
+    return t
+
+
+def t_MUL_OP(t):
+    r'\*'
+    return t
+
+
+def t_DIV_OP(t):
+    r'/'
+    return t
+
+
+def t_ASSIGN(t):
+    r'='
+    return t
+
+
+def t_COMP_GT(t):
+    r'>'
+    return t
+
+
+def t_COMP_LT(t):
+    r'<'
+    return t
+
+
 def t_IDENTIFIER(t):
     r'[A-Za-z_]\w*'
     if t.value in reserved_words:
         t.type = t.value.upper()
+    return t
+
+
+def t_STRING(t):
+    r'\"([^\"]*?[^\"]*?)\"'
+    return t
+
+
+def t_VARIABLE(t):
+    r'\$\w[\w\d]*'
     return t
 
 
@@ -71,3 +174,4 @@ if __name__ == "__main__":
         if not tok:
             break
         print("line %d: %s(%s)" % (tok.lineno, tok.type, tok.value))
+
